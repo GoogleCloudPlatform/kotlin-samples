14e6d0509966801c9c93264a5a28d894cf1b8b4f
Wisdom D'almeida <wdalmeida6@gmail.com>
2018-08-10 16:48:42 -0700

    Implemented tests

--- getting-started/src/main/kotlin/com/google/cloud/kotlin/emojify/EmojifyController.kt
+++ getting-started/src/main/kotlin/com/google/cloud/kotlin/emojify/EmojifyController.kt
@@ -26,6 +26,7 @@ import com.google.cloud.vision.v1.ImageSource
 import com.google.cloud.vision.v1.Image
 import com.google.cloud.vision.v1.Feature
 import com.google.cloud.vision.v1.Feature.Type
+import org.springframework.http.HttpStatus
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -41,6 +42,16 @@ import java.util.logging.Logger
 enum class Emoji {
     JOY, ANGER, SURPRISE, SORROW, NONE
 }
+
+val errorMessage = mapOf(
+    100 to "Other",
+    101 to "Slashes are intentionally forbidden in objectName.",
+    102 to "storage.bucket.name is missing in application.properties.",
+    103 to "Blob specified doesn't exist in bucket.",
+    104 to "blob ContentType is null.",
+    105 to "Size of responsesList is not 1.",
+    106 to "objectName is null."
+)
 
 // Returns best emoji based on detected emotions likelihoods
 fun bestEmoji(annotation: FaceAnnotation): Emoji {
@@ -59,17 +70,21 @@ fun bestEmoji(annotation: FaceAnnotation): Emoji {
     return Emoji.NONE
 }
 
-class EmojifyResponse(
+data class EmojifyResponse(
     val objectPath: String? = null,
     val emojifiedUrl: String? = null,
-    val status: Int = 200,
+    val statusCode: HttpStatus = HttpStatus.OK,
+    val errorCode: Int? = null,
     val errorMessage: String? = null
 )
 
 @RestController
 class EmojifyController(@Value("\${storage.bucket.name}") val bucketName: String, val storage: Storage, val vision: ImageAnnotatorClient) {
 
-    val log = Logger.getLogger(EmojifyController::class.java.name)
+    companion object {
+        val log: Logger = Logger.getLogger(EmojifyController::class.java.name)
+    }
+
     val emojiBufferedImage = mapOf(
         Emoji.JOY to stream("emojis/joy.png"),
         Emoji.ANGER to stream("emojis/anger.png"),
@@ -83,22 +98,24 @@ class EmojifyController(@Value("\${storage.bucket.name}") val bucketName: String
         return ImageIO.read(strm)
     }
 
-    fun errorResponse(status: Int, msg: String): EmojifyResponse {
-        log.severe(msg)
-        return EmojifyResponse(status = status, errorMessage = msg)
+    fun errorResponse(statusCode: HttpStatus, errorCode: Int = 100, msg: String? = null): EmojifyResponse {
+        val err = msg ?: errorMessage[errorCode]
+        log.severe(err)
+        return EmojifyResponse(statusCode = statusCode, errorCode = errorCode, errorMessage = err)
     }
 
     @GetMapping("/emojify")
     fun emojify(@RequestParam(value = "objectName") objectName: String): EmojifyResponse {
 
-        if (objectName.contains('/')) return errorResponse(400, "Slashes are intentionally forbidden in objectName.")
-        val bucket = storage.get(bucketName) ?: return errorResponse(500, "storage.bucket.name is missing in application.properties.")
+        if (objectName.isEmpty()) return errorResponse(HttpStatus.BAD_REQUEST, 106)
+
+        if (objectName.contains('/')) return errorResponse(HttpStatus.BAD_REQUEST, 101)
+
+        val bucket = storage.get(bucketName) ?: return errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 102)
         val publicUrl: String =
             "https://storage.googleapis.com/$bucketName/emojified/emojified-$objectName" // api response
-
-        val blob = bucket.get(objectName) ?: return errorResponse(400, "Blob specified doesn't exist in bucket.")
-
-        val imgType = blob?.contentType?.substringAfter('/') ?: return errorResponse(400, "Unable to read source image ContentType from GCS.")
+        val blob = bucket.get(objectName) ?: return errorResponse(HttpStatus.BAD_REQUEST, 103)
+        val imgType = blob.contentType?.substringAfter('/') ?: return errorResponse(HttpStatus.BAD_REQUEST, 104)
 
         // Setting up image annotation request
         val source = ImageSource.newBuilder().setGcsImageUri("gs://$bucketName/$objectName").build()
@@ -111,9 +128,9 @@ class EmojifyController(@Value("\${storage.bucket.name}") val bucketName: String
 
         // Calls vision api on above image annotation requests
         val response = vision.batchAnnotateImages(listOf(request))
-        if (response.responsesList.size != 1) return errorResponse(500, "Vision API didn't return a single response.")
+        if (response.responsesList.size != 1) return errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 105)
         val resp = response.responsesList[0]
-        if (resp.hasError()) return errorResponse(500, resp.error.message)
+        if (resp.hasError()) return errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 100, resp.error.message)
 
         // Writing source image to InputStream
         val imgBuff = stream(objectName)
@@ -131,16 +148,16 @@ class EmojifyController(@Value("\${storage.bucket.name}") val bucketName: String
             gfx.drawImage(imgEmoji, poly.xpoints[0], poly.ypoints[1], height, width, null)
         }
         // Writing emojified image to OutputStream
-        val baos = ByteArrayOutputStream()
-        ImageIO.write(imgBuff, imgType, baos)
+        val outputStream = ByteArrayOutputStream()
+        ImageIO.write(imgBuff, imgType, outputStream)
 
         // Uploading emojified image to GCS and making it public
         bucket.create(
             "emojified/emojified-$objectName",
-            baos.toByteArray(),
+            outputStream.toByteArray(),
             Bucket.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ)
         )
-        // Everything went well; we can return the public url!
+        // Everything went well!
         return EmojifyResponse(
             objectPath = "emojified/emojified-$objectName",
             emojifiedUrl = publicUrl
--- getting-started/src/test/kotlin/com/google/cloud/kotlin/emojify/EmojifyApplicationTests.kt
+++ getting-started/src/test/kotlin/com/google/cloud/kotlin/emojify/EmojifyApplicationTests.kt
@@ -16,15 +16,97 @@
 
 package com.google.cloud.kotlin.emojify
 
+import com.google.cloud.storage.Blob
+import com.google.cloud.storage.Storage
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
+import java.net.URL
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EmojifyApplicationTests {
+
+	@Value("\${storage.bucket.name}") lateinit var bucketName: String
+
+    @Autowired
+    lateinit var storage: Storage
+
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+	@Test
+	fun contextLoads() {
+        println(bucketName)
+	}
+
     @Test
-    fun contextLoads() {
+    fun `bad uri`() {
+        val response = testRestTemplate.getForEntity("/hi", EmojifyResponse::class.java) // providing a bad path
+        assertThat(response.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
     }
+
+    @Test
+    fun `no parameter`() {
+        val response = testRestTemplate.getForEntity("/emojify", EmojifyResponse::class.java) // required param missing
+        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+    }
+
+    @Test
+    fun `bad parameter name`() {
+        val response = testRestTemplate.getForEntity("/emojify?object=engineers.png", EmojifyResponse::class.java) // param should be objectName
+        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+    }
+
+    @Test
+    fun `blob does not exist`() {
+        // First making sure that object really doesn't exist
+        val blob: Blob? = storage.get(bucketName, "IDoNotExist")
+        assertThat(blob).isNull()
+
+        val response = testRestTemplate.getForEntity("/emojify?objectName=IDoNotExist", EmojifyResponse::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response?.body).isEqualTo(EmojifyResponse(statusCode = HttpStatus.BAD_REQUEST, errorCode = 103, errorMessage = errorMessage[103]))
+    }
+
+    @Test
+    fun `objectName contains slashes`() {
+        val response = testRestTemplate.getForEntity("/emojify?objectName=I/Can/Navigate/In/Your/Bucket", EmojifyResponse::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response?.body).isEqualTo(EmojifyResponse(statusCode = HttpStatus.BAD_REQUEST, errorCode = 101, errorMessage = errorMessage[101]))
+    }
+
+    @Test
+    fun `blob ContentType is not set`() {
+        val publicImage = URL("https://lh3.googleusercontent.com/2ONX_nVMyhMt62GrKOJj9yf6SHvD6T7QEGidCg4P3YeAh5m4nyKbbg3lUr_TR3GA09PVP5xjF_cfaOwj4mYGgg=w1614").readBytes()
+        val blob = storage.get(bucketName).create("google-diversity", publicImage)
+        assertThat(blob).isNotNull
+
+        val response = testRestTemplate.getForEntity("/emojify?objectName=google-diversity", EmojifyResponse::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response?.body).isEqualTo(EmojifyResponse(statusCode = HttpStatus.BAD_REQUEST, errorCode = 104, errorMessage = errorMessage[104]))
+        assertThat(blob.delete()).isTrue()
+    }
+
+    @Test
+    fun `source image processed and emojified image is public`() {
+        val publicImage = URL("https://lh3.googleusercontent.com/2ONX_nVMyhMt62GrKOJj9yf6SHvD6T7QEGidCg4P3YeAh5m4nyKbbg3lUr_TR3GA09PVP5xjF_cfaOwj4mYGgg=w1614").readBytes()
+        val blob = storage.get(bucketName).create("google-diversity-2", publicImage, "image/png")
+        assertThat(blob).isNotNull
+
+        val response = testRestTemplate.getForEntity("/emojify?objectName=google-diversity-2", EmojifyResponse::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response?.body?.statusCode).isEqualTo(HttpStatus.OK)
+
+        val emojifiedImage = URL(response?.body?.emojifiedUrl).readBytes()
+        assertThat(emojifiedImage).isNotNull()
+        assertThat(blob.delete()).isTrue()
+    }
+
 }
