99d707ad8e1514da0bfc625d91927ae854d17d87
Brent Shaffer <betterbrent@google.com>
2018-09-18 14:56:08 -0700

    Adds Ktor sample

---
+++ appengine/ktor/README.md
@@ -0,0 +1,4 @@
+# Ktor on Google Appengine Standard
+
+To deploy [Ktor](https://ktor.io/) to Google App Engine Standard, simply follow
+the [community tutorial](https://cloud.google.com/community/tutorials/kotlin-ktor-app-engine-java8).
---
+++ appengine/ktor/src/main/kotlin/HelloApplication.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.demo
+
+/* ktlint-disable no-wildcard-imports */
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.routing.*
+import kotlinx.html.*
+
+// Entry Point of the application as defined in resources/application.conf.
+// @see https://ktor.io/servers/configuration.html#hocon-file
+fun Application.main() {
+    // This adds Date and Server headers to each response, and allows custom additional headers
+    install(DefaultHeaders)
+    // This uses use the logger to log every call (request/response)
+    install(CallLogging)
+
+    routing {
+        // Here we use a DSL for building HTML on the route "/"
+        // @see https://github.com/Kotlin/kotlinx.html
+        get("/") {
+            call.respondHtml {
+                head {
+                    title { +"Ktor on Google App Engine Standard" }
+                }
+                body {
+                    p {
+                        +"Hello there! This is Ktor running on Google Appengine Standard"
+                    }
+                }
+            }
+        }
+        get("/demo") {
+            call.respondHtml {
+                head {
+                    title { +"Ktor on Google App Engine Standard" }
+                }
+                body {
+                    p {
+                        +"It's another route!"
+                    }
+                }
+            }
+        }
+    }
+}
