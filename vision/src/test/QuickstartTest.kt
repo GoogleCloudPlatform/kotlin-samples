3ee4e750f2b26c38c6b3620e8ab7228db52c2576
Brent Shaffer <betterbrent@google.com>
2018-09-19 13:43:03 -0700

    reorganizes src directories (#36)

--- vision/src/test/QuickstartTest.kt
+++ vision/src/test/QuickstartTest.kt
@@ -11,8 +11,10 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
+package com.google.vision
+
 import org.junit.Test
-import com.google.vision.main
 
 internal class QuickstartTest {
 
