b4cffa59628acf5f9061fad5a18833d69351b6a5
jmfayard <jmfayard@gmail.com>
2019-01-15 10:44:03 +0100

    Add and run Gradle bildSrcVersions
    $ ./gradlew buildSrcVersions
    Task :dependencyUpdates
    Task :buildSrcVersions
            new file:   buildSrc/build.gradle.kts
            new file:   buildSrc/.gitignore
            new file:   buildSrc/settings.gradle.kts
            new file:   buildSrc/src/main/kotlin/Libs.kt
            new file:   buildSrc/src/main/kotlin/Versions.kt
    
    Signed-off-by: Jean-Michel Fayard <jmfayard@gmail.com>

---
+++ appengine/ktor/buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,36 @@
+import kotlin.String
+
+/**
+ * Find which updates are available by running
+ *     `$ ./gradlew buildSrcVersions`
+ * This will only update the comments.
+ *
+ * YOU are responsible for updating manually the dependency version. */
+object Versions {
+    const val appengine: String = "1.9.60" // available: "1.9.71"
+
+    const val appengine_gradle_plugin: String = "1.3.4" // available: "1.3.5"
+
+    const val google_cloud_logging_logback: String = "0.60.0-alpha" 
+
+    const val de_fayard_buildsrcversions_gradle_plugin: String = "0.3.2" 
+
+    const val io_ktor: String = "0.9.4" // available: "1.1.1"
+
+    const val org_jetbrains_kotlin: String = "1.2.61" // available: "1.3.11"
+
+    /**
+     *
+     *   To update Gradle, edit the wrapper file at path:
+     *      ./gradle/wrapper/gradle-wrapper.properties
+     */
+    object Gradle {
+        const val runningVersion: String = "4.10"
+
+        const val currentVersion: String = "5.1.1"
+
+        const val nightlyVersion: String = "5.2-20190115022437+0000"
+
+        const val releaseCandidate: String = ""
+    }
+}
