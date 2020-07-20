ee99b4ea5a14e335131fc3d7e0dcb31f60939f1a
James Ward <james@jamesward.com>
2020-07-20 16:38:00 -0600

    add quarkus and micronaut

---
+++ run/micronaut-hello-world/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.72"
+    kotlin("kapt") version "1.3.72"
+    kotlin("plugin.allopen") version "1.3.72"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+
+    implementation("io.micronaut:micronaut-runtime:2.0.0")
+    implementation("io.micronaut:micronaut-http-server-netty:2.0.0")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
+    kapt("io.micronaut:micronaut-inject-java:2.0.0")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        javaParameters = true
+    }
+}
+
+application {
+    mainClassName = "hello.WebAppKt"
+}
+
+allOpen {
+    annotation("io.micronaut.aop.Around")
+}
+
+kapt {
+    arguments {
+        arg("micronaut.processing.incremental", true)
+        arg("micronaut.processing.annotations", "hello.*")
+        arg("micronaut.processing.group", "hello")
+        arg("micronaut.processing.module", "hello")
+    }
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("-XX:TieredStopAtLevel=1", "-Dcom.sun.management.jmxremote")
+
+    if (gradle.startParameter.isContinuous) {
+        systemProperties = mapOf(
+            "micronaut.io.watch.restart" to "true",
+            "micronaut.io.watch.enabled" to "true",
+            "micronaut.io.watch.paths" to "src/main"
+        )
+    }
+}
+
+tasks.replace("assemble").dependsOn("installDist")
+
+tasks.create("stage").dependsOn("installDist")
