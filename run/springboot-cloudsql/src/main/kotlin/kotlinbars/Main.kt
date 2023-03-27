1d7a6bb6d5aa84e592b2790abdb01ba8d2ea4728
James Ward <james@jamesward.com>
2023-03-27 16:21:28 -0600

    code formatting

--- run/springboot-cloudsql/src/main/kotlin/kotlinbars/Main.kt
+++ run/springboot-cloudsql/src/main/kotlin/kotlinbars/Main.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
-import java.util.*
+import java.util.Properties
 
 data class Bar(@Id val id: Long?, val name: String)
 
