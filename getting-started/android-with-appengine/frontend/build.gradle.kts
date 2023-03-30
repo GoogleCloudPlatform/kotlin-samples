959c511ebf1d44f4b7d3ff125c403a387939f3d9
James Ward <james@jamesward.com>
2023-03-30 13:21:45 -0600

    remove compose

--- getting-started/android-with-appengine/frontend/build.gradle.kts
+++ getting-started/android-with-appengine/frontend/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "7.4.2" apply false
-    id("org.jetbrains.compose") version "1.3.1" apply false
     kotlin("android") version "1.8.20" apply false
 }
 
