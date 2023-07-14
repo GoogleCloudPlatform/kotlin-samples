01bdaa54e2bb31807036d313f91d160b10f00dac
James Ward <james@jamesward.com>
2023-07-14 13:12:46 -0600

    bump google-cloud-storage

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -9,8 +9,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation("com.google.cloud:google-cloud-storage:2.23.0")
+    implementation("com.google.cloud:google-cloud-storage:2.24.0")
     testImplementation("junit:junit:4.13.2")
+    // see: https://github.com/googleapis/sdk-platform-java/pull/1832
+    modules {
+        module("com.google.guava:listenablefuture") {
+            replacedBy("com.google.guava:guava", "listenablefuture is part of guava")
+        }
+    }
 }
 
 kotlin {
